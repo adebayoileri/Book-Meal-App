4ecdc044bb3b741bc3402ab81f3429ce08350148
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2020-11-25 18:27:51 +0100

    fix: (spread connection string)

--- src/models/db.js
+++ src/models/db.js
@@ -3,18 +3,15 @@ import config from "../config/config";
 import dotenv from "dotenv";
 
 dotenv.config();
-const environmentVariable = process.env.NODE_ENV;
-console.log(environmentVariable)
 let pool;
 if(process.env.NODE_ENV  === "production"){
-  console.log("production")
-  let connectionString =
-    "postgres://oaprqywd:QpVzjaDz4UYVfMDJb2wC6G9G30i0g4Lb@suleiman.db.elephantsql.com:5432/oaprqywd";
+  // console.log("production")
+  let connectionString = config["production"];
   pool = new pg.Pool({
-    connectionString,
+    ...connectionString,
     rejectUnauthorized: false,
   });
-  console.log(connectionString);
+  // console.log(connectionString);
 }
 if (process.env.NODE_ENV === "development" || "test") {
   let connectionString = config["development"];
@@ -24,7 +21,7 @@ if (process.env.NODE_ENV === "development" || "test") {
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
   });
-  console.log(connectionString);
+  // console.log(connectionString);
 } 
 
 // console.log({...connectionString});
