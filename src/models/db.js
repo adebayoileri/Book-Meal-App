bde8c9b5ff659bcf1dfe472288a27fdbafe6705a
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2020-11-25 15:27:47 +0100

    fix: set production database

--- src/models/db.js
+++ src/models/db.js
@@ -1,24 +1,29 @@
-import pg from 'pg';
-import config from '../config/config';
-import dotenv from 'dotenv';
+import pg from "pg";
+import config from "../config/config";
+import dotenv from "dotenv";
 
 dotenv.config();
 let connectionString;
 const environmentVariable = process.env.NODE_ENV;
 
-if (environmentVariable === 'development' || 'test') 
-connectionString = config['development'];
-else {
-  connectionString = config['production'];
-}
-const pool = new pg.Pool({
-  ...connectionString,
+let pool;
+if (environmentVariable === "development" || "test") {
+  connectionString = config["development"];
+  pool = new pg.Pool({
+    ...connectionString,
     max: 20,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
-})
+    connectionTimeoutMillis: 2000,
+  });
+} else {
+  connectionString = process.env.DB_URL;
+  pool = new Pool({
+    connectionString,
+    rejectUnauthorized: false,
+  });
+}
 
 // console.log({...connectionString});
-pool.on('connect', () => { });
+pool.on("connect", () => {});
 
 module.exports = pool;
